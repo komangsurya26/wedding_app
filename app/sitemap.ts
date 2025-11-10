088e94536430591637f623a91c75014dffc427b0
Komang Surya <komangsuryasedana@gmail.com>
2025-11-10 13:00:26 +0800

    add seo

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: 'https://komangsuryaseadana.web.id',
+            url: 'https://komangsuryasedana.web.id',
             lastModified: new Date(),
             changeFrequency: 'yearly',
             priority: 1,
