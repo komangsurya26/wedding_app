14e85dbf07536ae97237827a7cea3116eb0fbaf2
Komang Surya <komangsuryasedana@gmail.com>
2025-11-12 17:56:34 +0800

    edit seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -78,8 +78,18 @@ export const metadata: Metadata = {
   title: "Resepsi Bali - Jasa Pembuatan Website Undangan Digital Bali",
   description:
     "Buat undangan digital elegan dengan Resepsi Bali. Desain eksklusif, fitur RSVP, dan tampilan profesional untuk pernikahan Anda.",
+  openGraph: {
+    siteName: "Resepsi Bali",
+    title: "Resepsi Bali - Jasa Pembuatan Website Undangan Digital Bali",
+    description:
+      "Buat undangan digital elegan dengan Resepsi Bali. Desain eksklusif, fitur RSVP, dan tampilan profesional untuk pernikahan Anda.",
+    url: "https://komangsuryasedana.web.id",
+    images: "https://komangsuryasedana.web.id/favicon/favicon-96x96.png",
+    type: "website",
+  },
   icons: {
     icon: "/favicon/favicon.ico",
+    apple: "/favicon/apple-touch-icon.png",
   },
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -5,47 +5,6 @@ import Creation from "@/app/components/Creation";
 import Feature from "@/app/components/Feature";
 import Footer from "@/app/components/Footer";
 
-export async function generateMetadata() {
-  const data = {
-    title: "Resepsi Bali - Jasa Pembuatan Website Undangan Digital Bali",
-    description:
-      "Buat undangan digital elegan dengan Resepsi Bali. Desain eksklusif, fitur RSVP, dan tampilan profesional untuk pernikahan Anda.",
-    url: "https://komangsuryasedana.web.id",
-  };
-
-  const imageUrl =
-    "https://masdianastudio.com/wp-content/uploads/2025/10/Undangan-Digital-Lumier_1-1.jpg";
-
-  return {
-    title: data.title,
-    description: data.description,
-    openGraph: {
-      siteName: "Resepsi Bali",
-      title: data.title,
-      description: data.description,
-      url: data.url,
-      images: [
-        {
-          url: imageUrl,
-          width: 1200,
-          height: 630,
-          alt: data.title,
-        },
-      ],
-      type: "website",
-    },
-    icons: {
-      icon: "/favicon.ico",
-      apple: "/favicon.png",
-    },
-    twitter: {
-      card: "summary_large_image",
-      title: data.title,
-      description: data.description,
-      images: [imageUrl],
-    },
-  };
-}
 
 export default function Home() {
   return (
