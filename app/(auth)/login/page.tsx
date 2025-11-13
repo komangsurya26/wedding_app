3cb6ea971fac12964cfc494c668173a6a5b7ed16
Komang Surya <komangsuryasedana@gmail.com>
2025-11-13 16:30:17 +0800

    fix suspense page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -5,7 +5,7 @@ export default function LoginPage() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <Suspense>
+        <Suspense fallback={<div>Loading...</div>}>
           <LoginForm />
         </Suspense>
       </div>
