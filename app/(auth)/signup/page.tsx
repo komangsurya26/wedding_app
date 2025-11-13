0ed62539f7b8bae301cc297eb690e96bcbe8c06e
Komang Surya <komangsuryasedana@gmail.com>
2025-11-13 16:31:55 +0800

    fix suspense page signup

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,10 +1,13 @@
 import { SignupForm } from "@/app/(auth)/signup/components/SignupForm";
+import { Suspense } from "react";
 
 export default function SignupPage() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <SignupForm />
+        <Suspense>
+          <SignupForm />
+        </Suspense>
       </div>
     </div>
   );
