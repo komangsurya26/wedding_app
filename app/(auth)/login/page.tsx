b3f6c0a9efede95507468424877c0dbaa4c045cc
Komang Surya <komangsuryasedana@gmail.com>
2025-11-14 21:26:25 +0800

    fix bug

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,14 +1,10 @@
 import { LoginForm } from "@/app/(auth)/login/components/LoginForm";
-import { SpinnerCustom } from "@/components/ui/spinner";
-import { Suspense } from "react";
 
 export default function LoginPage() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <Suspense fallback={<SpinnerCustom />}>
-          <LoginForm />
-        </Suspense>
+        <LoginForm />
       </div>
     </div>
   );
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,14 +1,10 @@
 import { SignupForm } from "@/app/(auth)/signup/components/SignupForm";
-import { SpinnerCustom } from "@/components/ui/spinner";
-import { Suspense } from "react";
 
 export default function SignupPage() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <Suspense fallback={<SpinnerCustom />}>
-          <SignupForm />
-        </Suspense>
+        <SignupForm />
       </div>
     </div>
   );
