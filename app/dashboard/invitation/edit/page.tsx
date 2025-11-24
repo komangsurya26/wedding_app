0246cb78f796e5898c39f6da716550008dabefd5
Komang Surya <komangsuryasedana@gmail.com>
2025-11-24 12:32:20 +0800

    minor userprovider

--- app/dashboard/invitation/edit/page.tsx
+++ app/dashboard/invitation/edit/page.tsx
@@ -1,19 +1,26 @@
 "use client";
 
-import React from "react";
-import { redirect, useSearchParams } from "next/navigation";
+import React, { useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 import { InvitationEditorMenu } from "./components/InvitationEditorMenu";
 
 export default function InvitationEditPage() {
   const searchParams = useSearchParams();
-
-  const invitationIdStr = searchParams.get("invitationId");
-  if (!invitationIdStr) return redirect("/dashboard/invitation");
-
-  const invitationId = Number(invitationIdStr);
+  const router = useRouter();
 
   const cloudName = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME as string;
 
+  const invitationIdStr = searchParams.get("invitationId");
+
+  useEffect(() => {
+    if (!invitationIdStr) {
+      router.push("/dashboard/invitation");
+    }
+  }, [invitationIdStr, router]);
+
+  if (!invitationIdStr) return null;
+
+  const invitationId = Number(invitationIdStr);
   return (
     <div className="h-full w-full">
       <InvitationEditorMenu invitationId={invitationId} cloudName={cloudName} />
