3e1531c2d30ef2527d0b3a30fcf5daae112feb08
Komang Surya <komangsuryasedana@gmail.com>
2025-11-24 20:04:30 +0800

    feat event and fix params create invitation

--- app/dashboard/invitation/page.tsx
+++ app/dashboard/invitation/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import React from "react";
+import React, { Suspense } from "react";
 import { Invitation as InvitationTypes } from "@/src/types";
 import { Invitation } from "./components/Invitation";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const invitations: InvitationTypes[] = [
   {
@@ -26,5 +27,24 @@ const invitations: InvitationTypes[] = [
 ];
 
 export default function InvitationPage() {
-  return <Invitation invitations={invitations} />;
+  return (
+    <Suspense fallback={<Fallback />}>
+      <Invitation invitations={invitations} />
+    </Suspense>
+  );
+}
+
+function Fallback() {
+  return (
+    <div className="w-full h-full">
+      <div className="mb-10 flex flex-row gap-3">
+        <Skeleton className="h-10 w-50" />
+        <Skeleton className="h-10 w-30" />
+      </div>
+      <div className="grid grid-cols-4 gap-3">
+        <Skeleton className="h-40 w-full" />
+        <Skeleton className="h-40 w-full" />
+      </div>
+    </div>
+  );
 }
