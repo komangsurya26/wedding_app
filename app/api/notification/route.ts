72fbbf0a8f8807269739d2b45a402afa5f01529a
Komang Surya <komangsuryasedana@gmail.com>
2025-12-02 10:22:42 +0800

    feat payment midtrans integration

--- app/api/notification/route.ts
+++ app/api/notification/route.ts
@@ -11,7 +11,7 @@ export async function POST(req: Request) {
     } catch (error) {
         return NextResponse.json({ ok: false, error: "invalid json" }, { status: 400 });
     }
-    const { order_id, status_code, gross_amount, transaction_status, signature_key } = payload;
+    const { order_id, status_code, gross_amount, transaction_status, signature_key, expiry_time } = payload;
 
     if (!verifySignature({ order_id, status_code, gross_amount, signature_key })) {
         return NextResponse.json({ ok: false, error: "invalid signature" }, { status: 401 });
@@ -30,15 +30,15 @@ export async function POST(req: Request) {
         // jika invitation id ada
         if (orders.invitation_id) {
             await supabase
-            .from("invitations")
-            .update({
-                status: "ACTIVE",
-                expires_at: addMonths(new Date(), 6).toISOString()
-            })
-            .eq("id", orders.invitation_id)
+                .from("invitations")
+                .update({
+                    status: "ACTIVE",
+                    expires_at: addMonths(new Date(), 6).toISOString() // now tambah 6 bulan
+                })
+                .eq("id", orders.invitation_id)
             return NextResponse.json({ ok: true, message: "Status settlement sukses" }, { status: 200 });
         }
-        
+
         // create invitations baru
         const insertInvitation = {
             user_id: orders.user_id,
@@ -53,10 +53,10 @@ export async function POST(req: Request) {
             .insert(insertInvitation)
         return NextResponse.json({ ok: true, message: "Status settlement sukses" }, { status: 200 });
     } else if (transaction_status === "pending") {
-        await supabase.from("orders").update({ status: "PENDING" }).eq("order_ref", order_id);
+        await supabase.from("orders").update({ status: "PENDING", expires_at: expiry_time }).eq("order_ref", order_id);
         return NextResponse.json({ ok: true, message: "Status pending sukses" }, { status: 200 });
     } else if (transaction_status === "deny" || transaction_status === "cancel" || transaction_status === "expire") {
-        await supabase.from("orders").update({ status: "FAILED", expires_at: new Date().toISOString() }).eq("order_ref", order_id);
+        await supabase.from("orders").update({ status: "FAILED" }).eq("order_ref", order_id);
         return NextResponse.json({ ok: true, message: "Status expired sukses" }, { status: 200 });
     }
 }
